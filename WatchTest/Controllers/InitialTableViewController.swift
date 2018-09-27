e25d5e89c01c2aaf22b1707193a12b92116a2053
Ramires Moreira <ramires.nas@gmail.com>
2018-09-27 09:07:21 -0300

    removing only remedies that not yet taken take

--- WatchTest/Controllers/InitialTableViewController.swift
+++ WatchTest/Controllers/InitialTableViewController.swift
@@ -122,14 +122,12 @@ class InitialTableViewController: UITableViewController {
         return true
     }
 
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let delete = UITableViewRowAction(style: .destructive, title: "delete") { (action, indexPath) in
+            self.remedies[indexPath.section].remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
+        return indexPath.section == 0 ? [delete] : []
     }
 
     /*
